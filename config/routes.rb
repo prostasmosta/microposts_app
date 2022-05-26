1223d7accfc9c2aaf0aa8fde563bdc38c26f90aa
prostasmosta <prostasmosta@gmail.com>
2022-05-26 09:09:11 +0300

    finished layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
 
-  resources :users
+
+  # resources :users
 
 end
