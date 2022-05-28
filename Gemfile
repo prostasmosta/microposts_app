089b983900ba410cfc9ff7664cfbe6ec84372d16
prostasmosta <prostasmosta@gmail.com>
2022-05-28 20:47:28 +0300

    added pics

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ gem 'tzinfo-data', platforms: %i[ mingw mswin x64_mingw jruby ]
 gem 'bootsnap', require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-# gem 'image_processing', '~> 1.2'
+gem 'image_processing', '~> 1.2'
 
 group :development, :test do
   gem 'debug', platforms: %i[ mri mingw x64_mingw ]
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,14 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_one_attached :picture
+
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 
   default_scope -> { order(created_at: :desc) }
+
+  def pic_thumb
+    return unless picture.content_type.in?(%w[image/jpg image/jpeg image/png image/gif])
+    picture.variant(resize_to_limit: [300,300]).processed
+  end
 end
