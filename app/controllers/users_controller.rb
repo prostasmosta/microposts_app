4ee6972c5e6858e3f2d0b211dce0c32a8105e25f
prostasmosta <prostasmosta@gmail.com>
2022-05-30 13:52:16 +0300

    ui fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,12 +62,10 @@ class UsersController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
