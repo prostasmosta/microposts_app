9b919eff68d564033c198c18739e4a0267c751ac
prostasmosta <prostasmosta@gmail.com>
2022-05-31 10:36:41 +0300

    fixed host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'bbq-events-manager.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'microposts-app1.herokuapp.com'}
 
   config.action_mailer.delivery_method = :mailjet
 
